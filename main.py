d8e68434ea3607dff0955955815b1d1d4e1d1bf8
matocool <83129369+matocool@users.noreply.github.com>
2022-11-14 18:09:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ def main(_user, _passwd, min_1, max_1):
 
     response = requests.post(url, data=data, headers=head).json()
     # print(response)
-    result = f"[{now}]\n账号：{user[:3]}****{user[7:]}\n修改步数（{step}）[" + response['message'] + "]\n"
+    result = f"[{now}]\n\n\n账号：{user[:3]}****{user[7:]}\\修改步数（{step}）\\[" + response['message'] + "]\n"
     # print(result)
     return result
 
@@ -230,7 +230,7 @@ def push_wx(desp=""):
     else:
         server_url = f"https://sc.ftqq.com/{sckey}.send"
         params = {
-            "text": '【小米运动步数修改】',
+            "text": '【运动步数修改推送】',
             "desp": desp
         }
 
